2dff4668ef052cfd0e19ea897c30d051ef4e8ab3
Hemant <hemusharma10001@gmail.com>
2026-02-20 06:45:54 +0530

    Phase 1 officially complete!

---
+++ bank-management-system/src/main/java/com/bms/bank_management_system/entity/Role.java
@@ -0,0 +1,18 @@
+package com.bms.bank_management_system.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name; // "CUSTOMER", "ADMIN"
+
+}
